f0dfaace632f14cd6eb41faa154756cc260e11b3
Jaemin Han <jaemin.han@nyu.edu>
2017-01-31 14:03:55 -0500

    react initial view complete

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# forecast
+# Forecast
+
+## It's Snowing! Wait...wait. Is it?
+
+## User Flow
+![React Setup](./public/initial_step.png)
---
+++ src/components/App.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './normalize.css';
+import style from './App.css';
+
+class App extends Component {
+
+  render() {
+    return (
+      <div id="app-container">
+      <link href="https://fonts.googleapis.com/css?family=Pangolin" rel="stylesheet" />
+        <header>
+          <h1>Main Page</h1>
+        </header>
+        <div id="center">
+          <p>Sup World.</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
