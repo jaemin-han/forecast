c86eb07b8a7afb2dd993f5300fd58bfede148cc4
Jaemin Han <jaemin.han@nyu.edu>
2017-02-01 15:03:32 -0500

    fetch api and weatherform component setup

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
 
-// mount our App at #container
+// Mount our App at #container
 ReactDOM.render(<App />, document.querySelector('#root-container'));
