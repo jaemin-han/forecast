3257b692f0328c36a77a3e857a06c88480faa0d1
Jaemin Han <jaemin.han@nyu.edu>
2017-02-07 11:36:55 -0500

    comment code and new branch in process

--- routes/weather.js
+++ routes/weather.js
@@ -6,13 +6,12 @@ const API_KEY = process.env.WEATHER_API_KEY;
 router.route('/weather')
   .get(jsonResults);
 
-// router.get('/weather', jsonResults);
+// Router.get('/weather', jsonResults);
 router.post('/weather', (req, res) => {
   console.log(req.body);
-  // get weather info
+  // Get weather info
   let zip = req.body.zip;
   console.log(zip);
-  // fetch(url).then().then().catch()
   fetch(`http://api.openweathermap.org/data/2.5/weather?zip=${zip},us&appid=${API_KEY}`)
   .then(r=>r.json())
   .then(weather => {
@@ -21,24 +20,6 @@ router.post('/weather', (req, res) => {
   })
   .catch(() => res.status(500)) // internal error, no weather sorry
 });
-
-
-// router.get('/weather', (req, res) => {
-//     // handling request from client for temperature info...
-
-//     fetch(`http://api.openweathermap.org/data/2.5/weather?zip=${this.state.value},us&appid=${API_KEY}`)
-//     .then(r=>r.json())
-//     .then(weather => {
-//       console.log('in fetch weather', weather)
-//       this. setState({
-//         weatherCity: weather.name,
-//         // weatherTemp:
-//       });
-//       console.log('in fetch states', this.state)
-//     })
-//     .catch(() => res.status(500)) // internal error, no weather sorry
-
-//   })
 
 module.exports = router;
 
--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const express = require('express');
 const logger = require('morgan');
 const path = require('path');
 const bodyParser = require('body-parser');
+
+// routes setup
+// indexRouter called in the index routes
 const weatherRouter = require('./routes/weather');
 
 const app = express();
@@ -12,11 +15,16 @@ const PORT = process.argv[2] || process.env.port || 3000;
 // This will parse our payload from fetch which is sent as a JSON object
 app.use(bodyParser.json());
 
+// Log request
 app.use(logger('dev'));
 
+// Set static file root folder
+// sets logging so that we can see what's happening
+// and sets static assets path - my styling
 app.use(express.static(path.join(__dirname, 'dist')));
 
-
+// creating index file
 app.use('/api', weatherRouter);
 
+// Listen on port for connections
 app.listen(PORT, () => console.log('Server Listening Alright!', PORT));
